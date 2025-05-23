45a595481d621d894acd0fe7ff7f657b204f0601
Anish Kumar Yadav <anish1091@Anishs-MacBook-Pro.local>
2025-05-23 18:43:48 +0545

    baclink in backlink js

--- ui/app/common/ui-helper/directives/backLinks.js
+++ ui/app/common/ui-helper/directives/backLinks.js
@@ -5,7 +5,7 @@ angular.module('bahmni.common.uiHelper')
      return {
          template: '<ul>' +
                         '<li ng-repeat="backLink in backLinks">' +
-                            '<a class="back-btn" ng-if="backLink.action" accesskey="{{backLink.accessKey}}" ng-click="closeAllDialogs();backLink.action()" id="{{backLink.id}}"> <span ng-bind-html="backLink.label"></span> </a>' +
+                            '<a class="back-btn" ng-if="backLink.action" accesskey="{{backLink.accessKey}}" ng-click="closeAllDialogs(); linkAction(homeBackLink.type, homeBackLink.value, homeBackLink.params)" id="{{backLink.id}}"> <span ng-bind-html="backLink.label"></span> </a>' +
                             '<a class="back-btn" ng-class="{\'dashboard-link\':backLink.image}" ng-if="backLink.url" accesskey="{{backLink.accessKey}}" ng-href="{{backLink.url}}" ng-click="closeAllDialogs()" id="{{backLink.id}}"  title="{{backLink.title}}"> ' +
                                 '<img ng-if="backLink.image" ng-src="{{backLink.image}}" onerror="this.onerror=null; this.src=\'../images/blank-user.gif\'"/>' +
                                 '<i ng-if="backLink.icon && !backLink.image" class="fa {{backLink.icon}}"></i></a>' +
