bc08300cf57590a4b70b2925570f6337add9bd48
Anish Kumar Yadav <anish1091@Anishs-MacBook-Pro.local>
2025-06-01 14:44:31 +0545

    Add log in bahmniApp

--- ui/app/adt/controllers/wardListController.js
+++ ui/app/adt/controllers/wardListController.js
@@ -11,14 +11,10 @@ angular.module('bahmni.adt')
                     $window.location = appService.getAppDescriptor().formatUrl(Bahmni.ADT.Constants.mfeIpdDashboardUrl, options, true);
                     console.log("IPD URL:", Bahmni.ADT.Constants.ipdDashboardUrl);
                     console.log("MFE IPD URL:", Bahmni.ADT.Constants.mfeIpdDashboardUrl);
-
-
                 } else {
                     $window.location = appService.getAppDescriptor().formatUrl(Bahmni.ADT.Constants.ipdDashboardUrl, options, true);
                     console.log("IPD URL:", Bahmni.ADT.Constants.ipdDashboardUrl);
                     console.log("MFE IPD URL:", Bahmni.ADT.Constants.mfeIpdDashboardUrl);
-
-
                 }
             };
             $scope.searchText = '';
